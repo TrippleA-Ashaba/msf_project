95b4e9123e000d8978b1428d1bffb87134a1b0ce
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-22 09:55:58 +0300

    Sign up and Login

--- config/urls.py
+++ config/urls.py
@@ -17,4 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("apps.core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
+    path("", include("apps.core.urls")),
+]
