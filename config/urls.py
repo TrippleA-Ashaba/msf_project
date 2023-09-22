40a0c40bd0a3aba19e2ce4c2f42605e17192e3fb
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-22 10:34:40 +0300

    Configure dev settings

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
     path("", include("apps.core.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
